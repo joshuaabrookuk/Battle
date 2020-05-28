735202796339d82c3488e87e3072d16eb24d0d36
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-05-28 18:46:22 +0100

    corrected typos - test now green

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -4,6 +4,6 @@ require 'sinatra'
 feature "Testing infrastructure working!" do
   scenario "display 'hello battle' " do
   visit '/'
-  expect(page).to have_content("hello battle")
+  expect(page).to have_content("Hello Battle!")
   end
 end
